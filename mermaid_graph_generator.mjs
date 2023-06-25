ed381086f8d84bb04424a57e149458f4e7a3564c
Rémi Even <remi.even.partner@decathlon.com>
2023-06-25 12:58:39 +0200

    add rich hunters event chain and js script to turn json event chains into mermaid flowchart

---
+++ mermaid_graph_generator.mjs
@@ -0,0 +1,45 @@
+import { readFileSync } from 'node:fs';
+import { exit } from 'node:process';
+
+// This script reads an event chain json file and outputs a mermaid flowchart based on it
+
+if (process.argv.length !== 3) {
+    console.error("example usage: node mermaid_graph_generator.mjs event_chains/rich_hunters.json");
+    exit(1);
+}
+
+let eventChain;
+try {
+    eventChain = JSON.parse(readFileSync("./" + process.argv[2]));
+} catch (err) {
+    console.error("error reading event chain file: " + err);
+    exit(1);
+}
+
+const transitions = Object.keys(eventChain.events).map(e => ({
+    event: e,
+    reachable: [
+        ...(eventChain.events[e].next || []).map(next => next.event),
+        ...(eventChain.events[e].choices || []).flatMap(choice => choice.next).map(next => next.event),
+    ],
+}));
+
+const nodeLines = transitions.map(transition => {
+    if (transition.event === "start") {
+        return "start[[start]]";
+    } else if (transition.reachable.length !== 0) {
+        return `${transition.event}(${transition.event})`;
+    }
+    return ""
+}).filter(l => l !== "")
+.map(l => "\t" +l);
+
+const arrowLines = transitions.filter(node => node.reachable.length !== 0)
+    .map(node => `${node.event} --> ${node.reachable.join(" & ")}`)
+    .map(l => "\t" +l);
+
+console.log([
+    "flowchart TD",
+    ...nodeLines,
+    ...arrowLines,
+].join("\n"));
