112d7b1dcba30acc3c9f83444417c73b04d8d1d3
Roman Acevedo <roman.acevedo62@gmail.com>
2023-07-15 18:44:44 +0200

    refactor(engine): extract duplicated code handling event stacks processing

--- src/event_store.rs
+++ src/event_store.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::embed::get_event_chains;
-use crate::event::{Event};
+use crate::event::Event;
 use crate::event_chain::EventChain;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -22,15 +22,15 @@ impl EventStore {
                 return Some(chain.events.get(&event_name).unwrap().clone());
             }
         }
-        return None
+        return None;
     }
 
     pub fn get_chain(self, event_name: String) -> Option<String> {
         for chain in self.event_chains {
             if chain.events.contains_key(&event_name) {
-                return Some(chain.title)
+                return Some(chain.title);
             }
         }
-        return None
+        return None;
     }
 }
