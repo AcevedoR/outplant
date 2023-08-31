b34bf640aa2e0001c9637facdf714c8190122485
Rémi Even <remi.even.partner@decathlon.com>
2023-08-31 13:52:41 +0200

    add autoSelect option in chains and 'plans' event chain

--- src/engine/chain.rs
+++ src/engine/chain.rs
@@ -5,12 +5,14 @@ use serde::{Deserialize, Serialize};
 use crate::engine::{effect::Effect, event::Event, trigger::Trigger};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Chain {
     pub(crate) title: String,
     pub(crate) cooldown: u32,
     pub(crate) trigger: Option<Trigger>,
     pub(crate) events: HashMap<String, Event>,
     pub(crate) effects: HashMap<String, Effect>,
+    pub(crate) auto_select: Option<bool>,
 }
 
 impl Chain {}
