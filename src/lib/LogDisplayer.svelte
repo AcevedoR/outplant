b5c3bd77b496110d1720b2d21bc2627ec2d85fea
Roman Acevedo <roman.acevedo62@gmail.com>
2024-04-17 18:53:15 +0200

    feat(ui): regroup entry lines by chain, highlight last line

--- src/lib/LogDisplayer.svelte
+++ src/lib/LogDisplayer.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-    export let lines: string[];
-</script>
+    export let linesByChain: { [key: string]: Array<string> } = {};
+    $: currentChain = getLastChain(linesByChain);
 
+    const getLastChain = (linesByChain: { [key: string]: Array<string> }) => {
+        let entries = Object.entries(linesByChain);
+        console.log(entries.map(([chainName, lines]) => lines.length));
+        if(entries.map(([chainName, lines]) => lines.length).flat().reduce((a,b) => a+b) > 1) {
+            return entries[entries.length-1][0];
+        }
+    };
+</script>
 <section>
-    <ul>
-        {#each lines as line}
-            <li class="log-displayer__entry">{line}</li>
-        {/each}
-    </ul>
+    {#each Object.entries(linesByChain) as [chainName, lines]}
+        <ul class="log-displayer__entry-group { (currentChain && currentChain === chainName) ? currentChain + ' highlight-group' : ''}">
+            {#each lines as line}
+                <li class="log-displayer__entry">{line}</li>
+            {/each}
+        </ul>
+    {/each}
 </section>
 
 <style>
@@ -19,5 +29,30 @@
 
     .log-displayer__entry:first-of-type {
         padding-top: 0;
+    }
+
+    .log-displayer__entry-group {
+        position: relative;
+    }
+
+    section ul.log-displayer__entry-group + ul.log-displayer__entry-group {
+        border-top: 1px solid var(--main-color-light);
+        padding-top: 20px;
+        margin-top: 20px
+    }
+
+    .highlight-group.log-displayer__entry-group .log-displayer__entry:last-of-type {
+        border: 1px solid var(--main-color-light);
+        padding: 5px;
+        margin-top: 20px
+    }
+
+    .highlight-group.log-displayer__entry-group .log-displayer__entry:last-of-type::before {
+        white-space: nowrap;
+        content: ">";
+        display: inline;
+        padding-left: 0;
+        position: absolute;
+        left: -15px;
     }
 </style>
