e02650169460b56956fa23b5cacd840a07483db7
Roman Acevedo <roman.acevedo62@gmail.com>
2024-04-18 23:56:23 +0200

    test: add simple playwright e2e test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as path from 'path';
 
-// https://vitejs.dev/config/
+/// <reference types="vitest" />
 export default defineConfig({
   plugins: [svelte()],
   base: './',
+  test: {
+    root: path.resolve(__dirname, './src'),
+  },
 })
